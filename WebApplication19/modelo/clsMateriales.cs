1efaca47646617e73ff31312bbbcc43ee4657ca5
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-20 16:52:03 -0600

    feat(modelos): place attributes for class materiales

---
+++ WebApplication19/modelo/clsMateriales.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication19.modelo
+{
+    public class clsMateriales
+    {
+        public int id {  get; set; }
+
+        public string nombre { get; set; }
+
+        public string descripcion { get; set; }
+
+        public string puntosporkg { get; set; }
+    }
+}
