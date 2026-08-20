6f0c156ed5d5504362d701db9c430cbfa3eff1e4
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-20 01:19:32 -0600

    feat(usuarios): display logged user info

--- WebApplication19/vista/Usuarios.aspx.cs
+++ WebApplication19/vista/Usuarios.aspx.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebApplication19.logica;
+using WebApplication19.modelo;
 
 namespace WebApplication19.vista
 {
-    public partial class Usuarios : System.Web.UI.Page
+    public partial class Usuarios : SecurePage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -18,8 +20,27 @@ namespace WebApplication19.vista
         }
         private void CargarUsuarioLogueado()
         {
-            rptBoxes.DataSource = logica.UsuariosLogica.ObtenerUsuarioLogueado();
-            rptBoxes.DataBind();
+            // Se llama a la logica para obtener el usuario logueado, se le envia el ID de SecurePage (UsuarioIdLogueado) y se obtiene el perfil del usuario logueado
+            clsUsuarios perfilUsuario = logica.UsuariosLogica.ObtenerUsuarioLogueado(this.UsuarioIdLogueado);
+
+            if(perfilUsuario != null )
+            {
+                // Se llena el label del inicio con el nombre del usuario logueado
+                lblNombreUsuario.Text = perfilUsuario.nombre;
+
+                // El repeater necesita una lista por fuerza, metemos el perfilUsuario en una lista para que funcione
+                List<clsUsuarios> listaParaRepeater = new List<clsUsuarios>();
+                listaParaRepeater.Add(perfilUsuario);
+
+                // Se enlaza el repeater con la lista que contiene el perfil del usuario logueado
+                rptBoxes.DataSource = listaParaRepeater;
+                rptBoxes.DataBind();
+            }
+            else
+            {
+                // Manejo de errores si no se encuentra el usuario logueado
+                lblNombreUsuario.Text = "Error al cargar el usuario logueado.";
+            }
         }
     }
 }
