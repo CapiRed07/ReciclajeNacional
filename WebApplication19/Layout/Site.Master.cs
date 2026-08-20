bb9e701452c86814795500dd65a7c80cb45334c3
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-20 15:54:42 -0600

    chore(master): create .cs file and place boilerplate

---
+++ WebApplication19/Layout/Site.Master.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication19.Layout
+{
+    public class SiteMaster : System.Web.UI.Page
+    {
+        public void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
