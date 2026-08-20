c88c440e732b449559192d9522f8e0f17d1ef7ef
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-20 04:43:44 -0600

    feat(registro): create btnRegistrar_Click

--- WebApplication19/vista/Registro.aspx.cs
+++ WebApplication19/vista/Registro.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebApplication19.logica;
+using WebApplication19.modelo;
 
 namespace WebApplication19.vista
 {
@@ -12,6 +14,33 @@ namespace WebApplication19.vista
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            // Instancia, se le pasan los valores de los campos del formulario al objeto
+            clsUsuarios nuevoRegistro = new clsUsuarios();
+            nuevoRegistro.nombre = txtNombre.Text;
+            nuevoRegistro.correo = txtCorreo.Text;
+            nuevoRegistro.provincia = ddlProvincia.SelectedValue;
+            nuevoRegistro.pwd = txtPwd.Text;
+
+            // Llamada al método para registrar el usuario
+            try
+            {
+                if(string.IsNullOrWhiteSpace(nuevoRegistro.nombre) || string.IsNullOrWhiteSpace(nuevoRegistro.correo) || string.IsNullOrWhiteSpace(nuevoRegistro.pwd))
+                {
+                    lblMensaje.Text = "Por favor, complete todos los campos obligatorios.";
+                    return;
+                }
+                UsuariosLogica.AgregarUsuario(nuevoRegistro);
+                lblMensaje.Text = "Usuario registrado correctamente.";
+
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = "Error al registrar el usuario: " + ex.Message;
+            }
         }
     }
 }
