4901aa9966a0efa9ac13e2520ff1a52e92054fe6
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-20 04:27:58 -0600

    feat(login): create btnLogin_Click

--- WebApplication19/vista/Login.aspx.cs
+++ WebApplication19/vista/Login.aspx.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using WebApplication19.logica;
+using WebApplication19.modelo;
 namespace WebApplication19.vista
 {
 	public partial class Login : System.Web.UI.Page
@@ -16,7 +17,33 @@ namespace WebApplication19.vista
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
+            // Crear una instancia de clsUsuarios con los datos ingresados por el usuario
+            clsUsuarios nuevoLogin = new clsUsuarios();
+			nuevoLogin.correo = txtCorreo.Text;
+			nuevoLogin.pwd = txtPassword.Text;
 
+            // Crear una instancia de UsuariosLogica para validar el login
+            UsuariosLogica nuevaValidacion = new UsuariosLogica();
+
+			try
+			{
+                // Se le pasa el objeto nuevoLogin a la función ValidarLogin para verificar si el login es correcto
+                if (nuevaValidacion.ValidarLogin(nuevoLogin))
+                {
+                    // Login exitoso
+                    Response.Redirect("Home.aspx");
+                }
+                else
+                {
+                    // Login fallido
+                    lblError.Text = "Correo o contraseña incorrectos.";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores
+                lblError.Text = "Ocurrió un error durante el inicio de sesión. Por favor, inténtelo de nuevo más tarde.";
+            }
         }
-    }
 }
