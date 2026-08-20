f6c267850e409c0953dcb5c1444033968e4c7b23
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-20 16:20:00 -0600

    feat(master.cs): hide navigation bar for some pages
    Adiotionally, logic to control cs behaviour, cs calls and master modification were perfomed to hide navigation bar for not logged-in-yet pages.

--- WebApplication19/Layout/Site.Master.cs
+++ WebApplication19/Layout/Site.Master.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using WebApplication19.logica;
 namespace WebApplication19.Layout
 {
-    public class SiteMaster : System.Web.UI.Page
+    public partial class SiteMaster : System.Web.UI.MasterPage
     {
         public void Page_Load(object sender, EventArgs e)
         {
-
+            // Se obtiene pagina actual
+            string paginaActual = Request.Url.AbsolutePath.ToLower();
+            globalNav.Visible = MasterLogica.NavOculta(paginaActual);
         }
     }
 }
---
+++ WebApplication19/logica/MasterLogica.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace WebApplication19.logica
+{
+    public class MasterLogica
+    {
+        // Paginas ocultas
+        private static readonly HashSet<string> PaginasOcultas = new HashSet<string>
+        {
+            "login.aspx",
+            "registro.aspx",
+            "unauthorized.aspx"
+        };
+        public static bool NavOculta(string paginaActual)
+        {
+            if(PaginasOcultas.Any(p => paginaActual.Contains(p)))
+            {
+                return false;
+            }
+            return true;
+
+        }
+    }
+}
