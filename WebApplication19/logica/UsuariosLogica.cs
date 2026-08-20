7c7989bb206b7072a169ad93894fa39036063dd7
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-19 23:47:47 -0600

    feat(logica): create add new user
    Method validates if the user is admin or not, handling provincia accordingly.

--- WebApplication19/logica/UsuariosLogica.cs
+++ WebApplication19/logica/UsuariosLogica.cs
@@ -56,5 +56,48 @@ namespace WebApplication19.logica
                 return new clsUsuarios();
             }
         }
+        public static int AgregarUsuario(clsUsuarios NuevoUsuario)
+        {
+            int retorno = 0;
+            SqlConnection Conn = new SqlConnection();
+            try
+            {
+                using (Conn = modelo.DBconn.obtenerConexion())
+                {
+                    //Se llama al stored procedure para agregar un usuario
+                    SqlCommand cmd = new SqlCommand("AgregarUsuario", Conn)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    // Agregamos los parámetros del usuario al comando
+                    cmd.Parameters.Add(new SqlParameter("@nombre", NuevoUsuario.nombre));
+                    cmd.Parameters.Add(new SqlParameter("@correo", NuevoUsuario.correo));
+                    cmd.Parameters.Add(new SqlParameter("@pwd", NuevoUsuario.pwd));
+                    cmd.Parameters.Add(new SqlParameter("@rol", NuevoUsuario.rol));
+
+                    // Validacion para la provincia, si es null o vacía para usuarios de rol "admin", se pasa como DBNull
+                    if (NuevoUsuario.rol == "admin" || string.IsNullOrEmpty(NuevoUsuario.provincia))
+                    {
+                        cmd.Parameters.Add(new SqlParameter("@provincia", DBNull.Value));
+                    }
+                    else
+                    {
+                        cmd.Parameters.Add(new SqlParameter("@provincia", NuevoUsuario.provincia));
+                    })
+                    retorno = cmd.ExecuteNonQuery(); //Devuelve el numero de filas afectadas, en este caso debería ser 1 si se insertó correctamente
+                }
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                // Manejo de errores
+                return -1; // Retorna -1 en caso de error
+            }
+            finally
+            {
+                Conn.Close();
+                Conn.Dispose();
+            }
+            return retorno;
+        }
     }
 }
