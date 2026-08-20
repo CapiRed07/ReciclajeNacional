0b9d06fe09963ff19f4af26dbb1a137739ff3dac
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-20 17:27:16 -0600

    feat(logica): get materiales table

--- WebApplication19/logica/MaterialesLogica.cs
+++ WebApplication19/logica/MaterialesLogica.cs
@@ -44,5 +44,56 @@ namespace WebApplication19.logica
             }
             return retorno;
         }
+        // Procedimiento para obtener todas las filas, los usuarios pueden usarlo libremente
+        public static clsMateriales ObtenerMateriales()
+        {
+            SqlConnection Conn = new SqlConnection();
+
+            // Se crea una lista para mantener a todos los materiales
+            List<clsMateriales> listaMateriales = new List<clsMateriales>();
+            // Fuera del try para retornarla si hay error
+            try
+            {
+                using (Conn = modelo.DBconn.obtenerConexion())
+                {
+                    //Stored procedure
+                    SqlCommand cmd = new SqlCommand("ConsultarMaterial", Conn)
+                    {
+                        CommandType = System.Data.CommandType.StoredProcedure // Tipo stored procedure
+                    };
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        // While en vez de if para leer todas las filas
+                        while(reader.Read())
+                        {
+                            clsMateriales materiales = new clsMateriales
+                            {
+                                id = reader.GetInt32(0),
+                                nombre = reader.GetString(1),
+                                descripcion = reader.GetString(2),
+                                puntosporkg = reader.GetDecimal(3),
+                            };
+
+                            // Pasamos los datos de cada material a la lista
+                            listaMateriales.Add(materiales);
+                        }
+
+                        // Los enviamos para el despliegue
+                        return listaMateriales;
+                    }
+                }
+            }
+            catch (SqlException Ex)
+            {
+                // Manejo de errores.
+                return listaMateriales;
+            }
+            finally
+            {
+                Conn.Close();
+            }
+            return listaMateriales;
+        }
     }
 }
