2d4cf1bb133f2ae16b530721e0b0067fa9976b0a
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-21 05:36:03 -0600

    feat(registros): add refresh, load, and query unique btns

--- WebApplication19/vista/Registros.aspx.cs
+++ WebApplication19/vista/Registros.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using WebApplication19.logica;
+using WebApplication19.modelo;
 
 namespace WebApplication19.vista
 {
@@ -12,7 +13,15 @@ namespace WebApplication19.vista
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                CargarRegistros();
+            }
+        }
+        private void CargarRegistros()
+        {
+            GridRegistros.DataSource = RegistrosLogica.ObtenerRegistros();
+            GridRegistros.DataBind();
         }
         protected void BtnGuardar_Click(object sender, EventArgs e)
         {
@@ -30,12 +39,39 @@ namespace WebApplication19.vista
 
         protected void BtnConsultar_Click(object sender, EventArgs e)
         {
+            if (!int.TryParse(TxtID.Text, out int parsedID))
+            {
+                // Manejo de errores
+                // Agregar lbl
+                return;
+            }
 
+            // Inicializacion limpia con objeto id
+            clsRegistros NuevaConsulta = new clsRegistros { id = parsedID };
+
+            // Consulta a la capa de logica
+            clsRegistros Consulta = RegistrosLogica.ConsultaRegistroporID(NuevaConsulta);
+
+            // Validacion de datos y asignacion al GridView
+            if (Consulta != null)
+            {
+                // Lista para que gridview lo pueda usar
+                GridRegistros.DataSource = new List<clsRegistros> { Consulta };
+                GridRegistros.DataBind();
+            }
+            else
+            {
+                // Limpiar el grid sino hay resultados
+                GridRegistros.DataSource = null;
+                GridRegistros.DataBind();
+                // lbl mensaje "no se encontro"
+            }
         }
 
         protected void BtnRefrescar_Click(object sender, EventArgs e)
         {
-
+            CargarRegistros();
+            TxtID.Text = "";
         }
     }
 }
