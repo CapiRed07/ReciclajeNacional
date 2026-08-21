78524ac021387f6de60caeb5ab29646e9140f7fc
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-21 08:54:35 -0600

    feat(logica): add cantidad to parameters

--- WebApplication19/logica/CanjeLogica.cs
+++ WebApplication19/logica/CanjeLogica.cs
@@ -32,6 +32,7 @@ namespace WebApplication19.logica
                      cmd.Parameters.Add(new SqlParameter("@RecompensaID", NuevoCanje.fkrecompensa));
                      cmd.Parameters.Add(new SqlParameter("@Fecha", NuevoCanje.fecha));
                      cmd.Parameters.Add(new SqlParameter("@PuntosUtilizados", NuevoCanje.puntosutilizados));
+                     cmd.Parameters.Add(new SqlParameter("@Cantidad", NuevoCanje.cantidad));
 
                      //Devuelve el numero de filas afectadas, 1 si tuvo exito
                      retorno = cmd.ExecuteNonQuery();
@@ -212,6 +213,7 @@ namespace WebApplication19.logica
                     cmd.Parameters.Add(new SqlParameter("@RecompensaID", CanjeModificar.fkrecompensa));
                     cmd.Parameters.Add(new SqlParameter("@Fecha", CanjeModificar.fecha));
                     cmd.Parameters.Add(new SqlParameter("@PuntosUtilizados", CanjeModificar.puntosutilizados));
+                    cmd.Parameters.Add(new SqlParameter("@Cantidad", CanjeModificar.cantidad));
 
                     // Conexion abierta y ejecucion
                     retorno = cmd.ExecuteNonQuery();
@@ -238,7 +240,7 @@ namespace WebApplication19.logica
             {
                 int puntosPorUnidad = Convert.ToInt32(recompensaSeleccionada.puntosnecesarios);
 
-                return puntosPorUnidad;
+                return puntosPorUnidad * Cantidad;
             }
 
             // Si por alguna razón no encuentra la recompensa, retorna 0 puntos para evitar inconsistencias
