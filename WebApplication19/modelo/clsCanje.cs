c3f3395d895c2dbe2bcb086444c8f84336ef14d9
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-21 07:41:24 -0600

    feat(modelo): create class and attributes for canje

---
+++ WebApplication19/modelo/clsCanje.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication19.modelo
+{
+    public class clsCanje
+    {
+        public int id {  get; set; }
+
+        public int fkusuario { get; set; }
+
+        public int fkrecompensa { get; set; }
+
+        public DateTime fecha { get; set; }
+
+        public int puntosutilizados { get; set; }
+    }
+}
