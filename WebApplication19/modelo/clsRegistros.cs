95aaf22054673ee34d8a0406c716b83f59f0523b
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-21 04:16:58 -0600

    feat(modelo): add class and attributes for registros

---
+++ WebApplication19/modelo/clsRegistros.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication19.modelo
+{
+    public class clsRegistros
+    {
+        public int id {  get; set; }
+
+        public int fkusuario { get; set; }
+
+        public int fkmaterial { get; set; }
+
+        public int fkcentro { get; set; }
+
+        public int cantidadkg { get; set; }
+
+        public DateTime fecha { get; set; }
+        
+        public int puntosobtenidos { get; set; }
+    }
+}
