cfc94abf3b1fc1d2c03058650cabd7d37b11acad
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-20 22:40:05 -0600

    feat(centros): create class and attributes centros

---
+++ WebApplication19/modelo/clsCentros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication19.modelo
+{
+    public class clsCentros
+    {
+        public int id {  get; set; }
+
+        public string nombre { get; set; }
+
+        public string provincia { get; set; }
+
+        public string direccion { get; set; }
+
+        public string horario { get; set; }
+    }
+}
