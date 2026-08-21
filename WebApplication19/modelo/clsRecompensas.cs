28242b0145c31f3aa7ae5277e0e65c2ae13a9cd5
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-21 03:13:22 -0600

    feat(modelo): create class and attributes for recompensas

---
+++ WebApplication19/modelo/clsRecompensas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication19.modelo
+{
+    public class clsRecompensas
+    {
+        public int id {  get; set; }
+
+        public string name { get; set; }
+
+        public string descripcion { get; set; }
+
+        public int puntosnecesarios { get; set; }
+
+        public int disponible { get; set; }
+    }
+}
