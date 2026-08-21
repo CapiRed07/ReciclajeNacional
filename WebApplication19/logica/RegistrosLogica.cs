ca64c7984710484783627f8df410d8db2bcbe94f
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-21 05:10:12 -0600

    feat(logica): display all registros

--- WebApplication19/logica/RegistrosLogica.cs
+++ WebApplication19/logica/RegistrosLogica.cs
@@ -49,4 +49,57 @@ namespace WebApplication19.logica
             return retorno;
         }
     }
-}
+    // Procedimiento para obtener todas las filas, los usuarios pueden usarlo libremente
+        public static List<clsRegistros> ObtenerRegistros()
+        {
+            SqlConnection Conn = new SqlConnection();
+
+            // Se crea una lista para mantener a todos los registros
+            List<clsRegistros> listaRegistros = new List<clsRegistros>();
+            // Fuera del try para retornarla si hay error
+            try
+            {
+                using (Conn = modelo.DBconn.obtenerConexion())
+                {
+                    //Stored procedure
+                    SqlCommand cmd = new SqlCommand("ConsultarRegistroReciclaje", Conn)
+                    {
+                        CommandType = System.Data.CommandType.StoredProcedure // Tipo stored procedure
+                    };
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        // While en vez de if para leer todas las filas
+                        while (reader.Read())
+                        {
+                            clsRegistros registros = new clsRegistros
+                            {
+                                id = reader.GetInt32(0),
+                                fkusuario = reader.GetInt32(1),
+                                fkmaterial = reader.GetInt32(2),
+                                fkcentro = reader.GetInt32(3),
+                                cantidadkg = reader.GetInt32(4),
+                                fecha = reader.GetDateTime(5),
+                                puntosobtenidos = reader.GetInt32(6)
+                            };
+
+                            // Pasamos los datos de cada registro a la lista
+                            listaRegistros.Add(registros);
+                        }
+
+                        // Los enviamos para el despliegue
+                        return listaRegistros;
+                    }
+                }
+            }
+            catch (SqlException Ex)
+            {
+                // Manejo de errores.
+                return listaRegistros;
+            }
+            finally
+            {
+                Conn.Close();
+            }
+        }
+    }
