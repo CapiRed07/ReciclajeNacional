3bf5d2eeb5231dca4cff2497b0c879efc9ae420e
CapiRed07 <237646127+CapiRed07@users.noreply.github.com>
2026-08-21 03:11:15 -0600

    chore(recompensa): fill boilerplate for page

--- WebApplication19/vista/Recompensas.aspx
+++ WebApplication19/vista/Recompensas.aspx
@@ -6,6 +6,13 @@
 
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
     <div>
-
+        <asp:GridView ID="GridRecompensas" runat="server"></asp:GridView>
+        <br />
+        <asp:Label ID="LblID" runat="server" Text="Ingrese un ID"></asp:Label>
+        <br />
+        <asp:TextBox ID="TxtID" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="BtnConsultar" runat="server" Text="Consultar por ID" OnClick="BtnConsultar_Click" />
+        <asp:Button ID="BtnRefrescar" runat="server" Text="Refrescar" OnClick="BtnRefrescar_Click" />
     </div>
 </asp:Content>
--- WebApplication19/vista/Recompensas.aspx.designer.cs
+++ WebApplication19/vista/Recompensas.aspx.designer.cs
@@ -13,5 +13,50 @@ namespace WebApplication19.vista
 
     public partial class Recompensas
     {
+
+        /// <summary>
+        /// GridRecompensas control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridRecompensas;
+
+        /// <summary>
+        /// LblID control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblID;
+
+        /// <summary>
+        /// TxtID control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtID;
+
+        /// <summary>
+        /// BtnConsultar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnConsultar;
+
+        /// <summary>
+        /// BtnRefrescar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnRefrescar;
     }
 }
